72b21d45235e85976d1242b65426b01aaaa87199
Timothy Nguyen <74280610+timothydknguyen@users.noreply.github.com>
2022-11-29 20:00:28 -0800

    Add files via upload

---
+++ MealMe/MealMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMeApp.swift
+//  MealMe
+//
+//  Created by Timothy Nguyen on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MealMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
